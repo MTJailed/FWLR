78c19fef30d39b613467442039b6c68c1950646a
MTJailed <marketing@jailed.ml>
2018-02-11 15:18:28 +0100

    Added more comments

--- Lecture 2/Classes/APIManager.m
+++ Lecture 2/Classes/APIManager.m
@@ -17,6 +17,7 @@
 @end
 @implementation APIManager
 
+//Load a framework or private framework into the runtime
 +(BOOL)loadFW:(NSString*)name private:(BOOL)isprivate {
     if(!isprivate) {
         return [[NSBundle bundleWithPath:[NSString stringWithFormat:@"/System/Library/Frameworks/%@.framework", name]] load];
@@ -44,6 +45,8 @@
 */
 
 
+//Hacky algorithm to dump all classes in the runtime and compare if they belong to a given framework or private framework
+//Returns a list of classes in a private framework
 +(NSArray*)dumpClasses:(NSString*)framework privateFW:(BOOL)pfw{
     
     uint count = 0;
@@ -71,39 +74,46 @@
     return [fwClasses componentsSeparatedByString:@"\n"];
 }
 
-
+//Dump all information from a given framework by calling all methods in this interface for the framework.
 +(NSDictionary*)dumpAllFromFramework:(NSString*)framework privateFW:(BOOL)pfw{
-    NSDictionary* data = [NSDictionary dictionary];
-    NSArray* classes = [APIManager dumpClasses:framework privateFW:pfw];
-    if(classes != nil) {
-        [data setValue:classes forUndefinedKey:@"Classes"];
-        for(int i = 0; i < classes.count;i++) {
-            Class c = NSClassFromString(classes[i]);
+    NSDictionary* data = [NSDictionary dictionary]; //Initialize a new dictionary
+    NSArray* classes = [APIManager dumpClasses:framework privateFW:pfw]; //Get an array of classes belonging to the given framework
+    if(classes != nil) { //If we actually found any classes
+        [data setValue:classes forUndefinedKey:@"Classes"]; //We add the array of classes to the dictionary
+        for(int i = 0; i < classes.count;i++) { //Then we add all properties and methods of each class to the dictionary
+            Class c = NSClassFromString(classes[i]); //Hacky way of recasting the class reference in the array to an actual class
             [data setValue:[APIManager dumpProperties:c] forUndefinedKey:@"Properties"];
             [data setValue:[APIManager dumpMethods:c] forUndefinedKey:@"Methods"];
         }
     }
-    return data;
+    return data; //Return the dictionary with classes, properties and methods
 }
 
+//Find all methods belonging to a class
 +(NSArray*)dumpMethods:(Class)class {
-    uint methodCnt = 0;
-    NSArray* methodArr = [NSArray array];
-    Method* methods = class_copyMethodList(class, &methodCnt);
+    uint methodCnt = 0; //might be an integer overflow vulnerability, but isn't exploitable anyway
+    NSArray* methodArr = [NSArray array]; //initialize a new array to add the methods to
+    //Create an array of methods and store the amount of methods in methodCnt.
+    Method* methods = class_copyMethodList(class, &methodCnt); //basically some kind of memcpy
     //printf("[%s Methods]:\n", class_getName(class));
-    for (uint i = 0; i < methodCnt; i++) {
-        Method method = methods[i];
+    for (uint i = 0; i < methodCnt; i++) { //foreach method
+        Method method = methods[i]; //Hacky way to cast the method reference in the array to an actual method
         //printf("\t- %s\n", sel_getName(method_getName(method)));
+        //Add the full selector of the method to the array of methods, because a selector is more precise with arguments.
         methodArr = [methodArr arrayByAddingObject:[NSString stringWithUTF8String:sel_getName(method_getName(method))]];
     }
     return methodArr;
 }
 
+//Hacky way of dumping all properties of a class and checking their types.
 +(NSArray*)dumpProperties:(Class)class{
-    uint count = 0;char* propertyType = "";
+    
+    uint count = 0;
+    char* propertyType = "";
     objc_property_t *properties = class_copyPropertyList(class, &count);
     //printf("[%s Properties]:\n", class_getName(class));
     NSArray* propertyArr = [NSArray array];
+    
     for(uint i = 0; i < count; i++) {
         if([[NSString stringWithUTF8String:property_getAttributes(properties[i])] containsString:@"NSString"]) {
             propertyType = "NSString";
@@ -166,61 +176,83 @@
         propertyArr = [propertyArr arrayByAddingObject:[NSString stringWithFormat:@"(%@) %@",[NSString stringWithUTF8String:propertyType], [NSString stringWithUTF8String:property_getName(properties[i])]]];
         propertyType = "";
     }
-    printf("[iVars]:\n");
+    
+    //printf("[iVars]:\n");
+    
     uint ivarsCnt;
     Ivar* ivars = class_copyIvarList(class, &ivarsCnt);
+    
     for (uint i = 0; i < ivarsCnt; i++) {
         char* ivar = (char*)ivar_getName(ivars[i]);
         propertyArr = [propertyArr arrayByAddingObject:[NSString stringWithFormat:@"(iVar) %s", ivar]];
     }
+    
     return propertyArr;
 }
 
 +(id) tryCallMethod:(NSString*)ClassName methodName:(NSString*)methodName {
+   
     Class tryClass = NSClassFromString(ClassName);
+    
     if(tryClass != nil) {
         @try {
             if([methodName isEqualToString:@"dealloc"]) {
                 return @"Failed to call method.\n(Reason: NULL Dereference)";
             }
+            
             id result = [[tryClass alloc] performSelector:NSSelectorFromString(methodName)];
+            
             if(result != nil || ![result isEqual:@""] || ![result isEqualToString:@""]) {
                 return result;
             } else {
                 return @"Method called but is probably void";
             }
+            
         } @catch(NSException *ex) {
             return [NSString stringWithFormat:@"Failed to call method.\n(Reason: %@)", ex.reason];
         }
+        
     } else {
         return @"Failed to call method.\n(Reason: Class is null.)";
     }
+    
 }
 
 + (NSString*) tryReadProperty:(NSString*)ClassName property:(NSString*)property{
     @try {
+        
         Class tryClass = NSClassFromString(ClassName);
+        
         if(tryClass == nil) {
             return @"Failed to read property\n(Reason: Class is null.)";
         } else {
+            
             id instance = [[tryClass alloc] valueForKey:property];
             NSString* result = [NSString stringWithFormat:@"%@",instance];
+            
             if(result == nil || [result isEqualToString:@"(null)"]) {
+                
                 instance = [tryClass valueForKey:property];
                 result = [NSString stringWithFormat:@"%@",instance];
+                
                 if(result == nil || [result isEqualToString:@"(null)"]) {
                     return @"Failed to read property\n(Reason: read returned null.)";
                 } else {
                     return result;
                 }
+            
             } else {
                 return result;
             }
+            
         }
+        
     } @catch(NSException *ex) {
+        
         if([ex.name containsString:@"NSUnknownKeyException"]) {
             return @"Failed to read property\n(Reason: read returned null.)";
         }
+        
         return [NSString stringWithFormat:@"Failed to read property\n(Reason: %@ %@.)",ex.name , ex.reason];
     }
 }
